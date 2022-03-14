2e1022ff4c4be4821e4aac6030eab8c135884aaa
Umanda Jayobandara <umanda@ifonix.io>
2022-03-14 20:59:34 +0530

    widgets

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
+import './home.scss';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
-import './home.scss';
+import Widget from '../../components/widget/Widget';
 
 const Home = () => {
   return (
@@ -8,6 +9,12 @@ const Home = () => {
       <Sidebar />
       <div className="home-container">
         <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
       </div>
     </div>
   );
